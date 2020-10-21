1a2f90ed369e76dc5c800880cd76a25a2d52d2a6
sookyung93 <i.sookyung93@gmail.com>
2020-10-22 00:11:59 +0900

    Updates

---
+++ precache-manifest.a856f00099af41e2ea3eee55d5037649.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4199e0a495c0c6bd2a8c09bfa60bdb57",
+    "url": "/habit-tracker/index.html"
+  },
+  {
+    "revision": "7e101a35313db80b129b",
+    "url": "/habit-tracker/static/css/main.8b73e418.chunk.css"
+  },
+  {
+    "revision": "bdac50018c0a7f3ef778",
+    "url": "/habit-tracker/static/js/2.b8c13bcf.chunk.js"
+  },
+  {
+    "revision": "76aa3a6712dcb2b7248a77b49e0cd963",
+    "url": "/habit-tracker/static/js/2.b8c13bcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e101a35313db80b129b",
+    "url": "/habit-tracker/static/js/main.562966c0.chunk.js"
+  },
+  {
+    "revision": "5d759b3fde7c32f59156",
+    "url": "/habit-tracker/static/js/runtime-main.0e3ad41c.js"
+  }
+]);
